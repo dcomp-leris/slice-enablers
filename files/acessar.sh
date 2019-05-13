18736758ac5fdbe1b6eeff7195af28404727d918
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-05-13 10:21:58 -0300

    updating acessar.sh

--- files/acessar.sh
+++ files/acessar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i cloud_ufscar_rsa.dms ubuntu@`cat lista-vms.txt | grep -w vm-$1 | cut -d " " -f 2`
+ssh -i cloud_ufscar_rsa.dms ubuntu@`cat lista-vms.txt | grep -w vm-$1 | cut -d " " -f 3`
